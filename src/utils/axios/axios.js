1e615408328cbfb4c3d23a5cec5168cd6de25bb3
tatarynrm <tatarynrm@gmail.com>
2023-02-21 16:47:31 +0200

    dotenv

--- src/utils/axios/axios.js
+++ src/utils/axios/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 import env from "react-dotenv";
 const instance = axios.create({
-  baseURL: "https://api.tatarynmira.pp.ua",
-  // baseURL: "http://localhost:8800",
+  // baseURL: "https://api.tatarynmira.pp.ua",
+  baseURL: "http://localhost:8800",
 });
 
 instance.interceptors.request.use((config) => {
